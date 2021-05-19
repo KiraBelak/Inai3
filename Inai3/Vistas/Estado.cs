d4da596ce331ed6e85d045df9b4ea835f6d05635
Orlando Gallegos <gallegosorlando18@gmail.com>
2021-05-19 12:44:26 -0600

    Estados Lista(Falta agregar navegacion a botones)

---
+++ Inai3/Vistas/Estado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inai3.Vistas
+{
+    public class Estado
+    {
+        public string Name { get; set; }
+        public string Ubicacion { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
