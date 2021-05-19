a68601f39fea08724df383b110cd694dca39d789
Omar <ponchomu25x@gmail.com>
2021-05-19 17:02:17 -0600

    Parte Completa partidos politicos

--- Inai3/Vistas/Partidos.xaml.cs
+++ Inai3/Vistas/Partidos.xaml.cs
@@ -3,18 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Inai3.NewFolder
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Partidos : ContentView
+    public partial class Partidos : ContentPage
     {
         public Partidos()
         {
             InitializeComponent();
+
+
+        }
+
+        private void PRI_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MORENA_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PAN_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PT_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ALI_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MVC_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VERDE_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void COALI_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PEST_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PRD_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PBC_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RSP_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FPM_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
