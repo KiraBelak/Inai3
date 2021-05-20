b7ec30087ea4d69699c160015477461699a32415
Kaleb Rodriguez <kalebchivas@hotmail.com>
2021-05-20 02:04:32 -0600

    Chefsito partidos lista

---
+++ Inai3/Vistas/PartidosListado.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inai3.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PartidosListado : ContentPage
+    {
+        public PartidosListado()
+        {
+            InitializeComponent();
+        }
+    }
+}
