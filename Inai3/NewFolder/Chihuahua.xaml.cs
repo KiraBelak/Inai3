4bb6d9a5334eb77e13b3b96a331d321f77394c21
Kaleb Rodriguez <kalebchivas@hotmail.com>
2021-05-20 12:16:03 -0600

    Medio Base de datos

---
+++ Inai3/NewFolder/Chihuahua.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inai3.NewFolder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Chihuahua : ContentPage
+    {
+        public Chihuahua()
+        {
+            InitializeComponent();
+        }
+    }
+}
